da04b173d4e80bf9436f1205e96411a676d0e223
jinu5715 <163946884+jinu5715@users.noreply.github.com>
2024-04-02 12:26:49 +0900

    Add files via upload

---
+++ d807.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int j;
+    printf("원하는 단을 입력하세요.\n");
+    scanf ("%d", &j);
+
+    for (int i=1; i <= 9; i++)
+    {
+        printf("%d * %d = %d\n", j, i, i*j);
+    }
+}
